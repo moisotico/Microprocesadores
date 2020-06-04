5b77bc59680d4ff1af8352c22c8cb16972cc73ba
Moises Campos Zepeda <moiso.camposcr@gmail.com>
2020-06-04 02:58:33 -0600

    BUSCAR working
    BUSCAR and RAIZ subroutine working,
    LEER_CANT and Print_RESULT pending.

--- Tareas/T3/Codigo/raiz_de_una_tabla.asm
+++ Tareas/T3/Codigo/raiz_de_una_tabla.asm
@@ -9,7 +9,7 @@
 ; $1000,  CANT en $1001, CONT en $1002, y ENTERO en $101
 
 ; Subroutine direction pointers
-PRINTF:     equ     $EE88
+PrintF:     equ     $EE88
 GETCHAR:    equ     $EE84
 PUTCHAR:    equ     $EE86
 
@@ -24,7 +24,7 @@ CANT:       ds      1
 CONT:       ds      1
 
             org         $1010
-ENTERO:     ds      100
+ENTEROS:    ds      100
 
             org         $1020
 DATOS:      db      4,9,18,4,27,63,12,32,36,15
@@ -40,68 +40,113 @@ TEMP_VAR2:  ds      2
 
             org         $1100
 ; Messages
-MESS0:     fcc "IE0623: Microprocesadores. Tarea #3. Elaborada por Moises Campos"
-            db CR,LF,FIN 
+MSG0:       fcc "IE0623: Microprocesadores. Tarea #3. Elaborada por Moises Campos"
+            fcb CR,CR,LF,FIN 
 
-MESS1:      fcc "Ingrese el valor de cant Entre 1 y 99"
-            db CR,LF,FIN
+MSG1:       fcc "Ingrese el valor de cant Entre 1 y 99"
+            fcb CR,CR,LF,FIN
 
-MESS2:      fcc "Cantidad de valores encontrados %i"
-            db CR,LF, FIN
+MSG2:       fcc "Cantidad de valores encontrados %i"
+            fcb CR,CR,LF,FIN
 
-MESS3:      fcc "Entero: "
-            db FIN
+MSG3:       fcc "Valores en Entero: +"
+            fcb FIN
 
 ; *****************************************************************************
 ;                               Main Program
 ; *****************************************************************************
             org             $2000
-            
+
+        ; Stack place 
+;            lds     #$3BFF
+;            ldx     #0
+;            ldd     #MSG0
+;            jsr     [PrintF,X]
+
             clra
             clrb
-            ;jsr     LEER_CANT
-
-            
             jsr     LEER_CANT
+
+            jsr     BUSCAR
+        ;   jsr     Print_RESULT
+        ; End of Program
+            bra     *
+
 
 
 ; *****************************************************************************
 ;                           Subroutine  LEER_CANT
 ; *****************************************************************************
 LEER_CANT:
-        ; SP stack pull
-            puly
-        ; CANT = $2 meanwhile
+        ;CANT = $2 meanwhile
+        ldaa    #$FF
+        staa    CANT
+        ldx     #0
+
+;            lds     #$3BFF
+;            ldd     #MSG1
+LOOP_CANT1:
+;            jsr     [PrintF,X]
+;            ldx     #0
+LOOP_CANT2:
+    ; TODO: Subroutine GETCHAR
+    
 
         ;TODO: Routine
-
         ; SP stack push
-            pshy
-            rts 
+        rts 
 
 
 ; *****************************************************************************
 ;                           Subroutine  BUSCAR
 ; *****************************************************************************
-        ; SP stack pull
-            puly
+BUSCAR:
+            ldx         #DATOS
+            ldy         #CUAD
+            clra
+            clrb
 
-        ;TODO: Routine
+LOOP_BUSCAR:
+        ; A = (CANT) || A = (LONG)
+            cmpa        CANT
+            beq         RETURN_BUSCAR
+            cmpa        LONG
+            beq         RETURN_BUSCAR
+        ; Stack A counter
+            psha                    
+            ldaa        0,X
+            ldab        0,Y
+            cba
+            beq         MATCH
+            cmpb        #$E1
+        ; No Match        
+            beq         INCREASE
+            iny
+            pula
+            jmp         LOOP_BUSCAR
 
-        ; Test 1
-            ldab        #4
-            pshb
+MATCH:      
+            pshx      
+            psha
             jsr         RAIZ
-            pulb
+            pula
+            pulx
+            sty         TEMP
+            ldy         #ENTEROS
+            ldab        CONT
+            staa        B,Y        
+            inc         CONT
+            jmp         INCREASE    
 
-        ; Test 2
-            ldab        #9
-            pshb
-            jsr         RAIZ
-            pulb
+INCREASE:
+            inx
+            ldy         #CUAD
+            pula
+            inca
+            jmp         LOOP_BUSCAR
 
-        ; SP stack push
-            pshy
+
+RETURN_BUSCAR:
             rts 
 
 ; *****************************************************************************
@@ -118,17 +163,17 @@ RAIZ:
 
 BABYLON:
         ; b value of algorithm
-            abx
+            leax        D,X
             xgdx
             lsrd
-            std         TEMP_VAR2
-
-        ; h value of algorithm
-            ldx         TEMP_VAR1        
+            xgdx
+            stx         TEMP_VAR2
+        ; h value of algorithm, X = TEMP_VAR1 / b value
+            ldd         TEMP_VAR1        
             idiv
             ldd         TEMP_VAR2
         ; b = h ?
-            cmpd        0,J
+            cpx         TEMP_VAR2
             beq         RETURN_RAIZ
             jmp         BABYLON
 
